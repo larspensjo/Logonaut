d77487d11f1ec05d8db64166c9b7280f57b66bd0
Lars Pensjö <lars.pensjo@gmail.com>
2025-02-28 08:10:22 +0100

    Enable immersive dark mode for MainWindow

--- src/Logonaut.UI/MainWindow.xaml.cs
+++ src/Logonaut.UI/MainWindow.xaml.cs
@@ -1,14 +1,29 @@
 ﻿using Logonaut.Theming;
 using System;
+using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Interop;
 
 namespace Logonaut.UI
 {
     public partial class MainWindow : Window
     {
+        const int DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
+
+        [DllImport("dwmapi.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+
         public MainWindow()
         {
             InitializeComponent();
+            this.SourceInitialized += MainWindow_SourceInitialized;
+        }
+
+        private void MainWindow_SourceInitialized(object sender, EventArgs e)
+        {
+            var hwnd = new WindowInteropHelper(this).Handle;
+            int useImmersiveDarkMode = 1; // 1 to enable dark mode
+            DwmSetWindowAttribute(hwnd, DWMWA_USE_IMMERSIVE_DARK_MODE, ref useImmersiveDarkMode, sizeof(int));
         }
 
         private void OnSwitchToDarkTheme(object sender, RoutedEventArgs e)
